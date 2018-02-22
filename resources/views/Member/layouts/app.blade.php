0d3b6dc768b113bb943489d9f04a0976bfc9e160
laserps <mosaklang@gmail.com>
2018-02-22 14:06:25 +0700

    fix css

--- resources/views/Member/layouts/app.blade.php
+++ resources/views/Member/layouts/app.blade.php
@@ -17,8 +17,8 @@
 
     <!-- Plugin -->
     <!-- OFFICEDD CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/member/assets/css/main.css">
-    <link rel="stylesheet" type="text/css" href="assets/member/assets/css/live-chat.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/member/assets/css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/member/assets/css/live-chat.css')}}">
     
     <!-- end header -->
 
