26c3cf2c61fe9befcea3dd1cf6cf34ae8ca967a7
wasink1995 <31298442+wasink1995@users.noreply.github.com>
2018-02-27 13:40:15 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -21,50 +21,53 @@ Route::get('/logout',function(){
 
 
 Route::get('/home', 'HomeController@index')->name('home');
-    Route::get('/', function(){
-        return view('welcome');
-    });
+
+Route::get('/', function(){
+    return view('welcome');
+});
+
 Route::group(['middleware' => 'member'], function(){
     // Route::get('/', function(){
     //     return view('welcome');
     // });
     //Route::resource('/',);
 
-Route::get('/main', function(){
-    return view('Member.main');
-});
-Route::get('/friend', function(){
-    return view('Member.friend');
-});
-Route::get('/album', function(){
-    return view('Member.album');
-});
-Route::get('/album_edit', function(){
-    return view('Member.album_edit');
-});
-Route::get('/photo', function(){
-    return view('Member.photo');
-});
-Route::get('/edit_profile', function(){
-    return view('Member.edit_profile');
+    Route::get('/main', function(){
+        return view('Member.main');
+    });
+    Route::get('/friend', function(){
+        return view('Member.friend');
+    });
+    Route::get('/album', function(){
+        return view('Member.album');
+    });
+    Route::get('/album_edit', function(){
+        return view('Member.album_edit');
+    });
+    Route::get('/photo', function(){
+        return view('Member.photo');
+    });
+    Route::get('/edit_profile', function(){
+        return view('Member.edit_profile');
+    });
+
+    Route::get('/wall', 'Member\FetchController@wall');
+    Route::post('/post', 'Member\PostController@store');
+    Route::post('/reply', 'Member\ReplyController@store');
+    Route::post('/comment', 'Member\CommentController@store');
+    Route::post('/report', 'Member\ReportController@store');
+    Route::get('/find_report/{post_id}', 'Member\ReportController@find');
+    Route::get('/profile', 'Member\UserController@index');
+    Route::post('/profile', 'Member\UserController@store');
+    Route::get('/info', 'Member\UserController@info');
+    Route::get('/search_place','Member\UserController@search_place');
 });
 
-Route::get('/wall','Member\FetchController@wall');
-Route::post('/post', 'Member\PostController@store');
-Route::post('/reply', 'Member\ReplyController@store');
-Route::post('/comment', 'Member\CommentController@store');
-Route::post('/report', 'Member\ReportController@store');
-Route::get('/find_report/{post_id}', 'Member\ReportController@find');
-Route::get('/profile', 'Member\UserController@index');
-Route::post('/profile', 'Member\UserController@store');
-Route::get('/info', 'Member\UserController@info');
-Route::get('/search_place','Member\UserController@search_place');
-
-Route::group(['middleware' => 'member'], function(){
+/*Route::group(['middleware' => 'member'], function(){
     Route::get('/first', function(){
         return view('Member.main');
     });
-});
+});*/
 
 // Route::get('/countstr',function(){
 //     $str ="https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/20108240_1613406825336653_8344623303259921044_n.jpg?oh=6f22318a227432fdc7068aa8a23270b6&oe=5B0C2239";
