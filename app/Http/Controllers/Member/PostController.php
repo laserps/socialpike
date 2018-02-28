15e1de4eed74776f5201623eecb64296c2448c9b
wasink1995 <31298442+wasink1995@users.noreply.github.com>
2018-02-28 17:42:44 +0700

    profile

--- app/Http/Controllers/Member/PostController.php
+++ app/Http/Controllers/Member/PostController.php
@@ -27,15 +27,24 @@ class PostController extends Controller
     public function like($id,$user_id){
         $user_id = intval($user_id);
         $like = Post::where('id',$id)->select('liked')->first();
-        $like = json_decode($like->liked);
-        if(in_array($user_id, $like)){
-            $like = array_diff($like,[$user_id]);
-            $count = count($like);
+        if($like->liked!=null){
+            $like = json_decode($like->liked);
+            if(in_array($user_id, $like)){
+                $like = array_diff($like,[$user_id]);
+                $result['count'] = count($like);
+            }else{
+                array_push($like, $user_id);
+                $result['count'] = count($like);
+                $result['status'] = 1;
+            }
+            $data['liked'] = json_encode($like);
         }else{
-            array_push($like, $user_id);
-            $count = count($like);
+            $data['liked'] = array();
+            array_push($data['liked'], $user_id);
+            $result['count'] = count($data['liked']);
+            $result['status'] = 0;
+            $data['liked'] = json_encode($data['liked']);
         }
-        $data['liked'] = json_encode($like);
         \DB::beginTransaction();
         try {
             Post::where('id',$id)->update($data);
@@ -43,7 +52,7 @@ class PostController extends Controller
         } catch (Exception $e){
             \DB::rollBack();
         }
-        return $count;
+        return $result;
     }
 
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,77 +1,198 @@
- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
+<!doctype html>
+<html lang="en">
+    <title>Social</title>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="keywords" content="สื่อสังคมออนไลน์">
+        <meta name="description" content="สื่อสังคมออนไลน์">
+        <meta name="robots" content="index, follow, all">
+        <meta name="author" content="">
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/member/SocialPikeHTML/node_modules/bootstrap/dist/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/member/SocialPikeHTML/node_modules/font-awesome/css/font-awesome.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/member/SocialPikeHTML/assets/css/live-chat.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/member/SocialPikeHTML/assets/css/main.css')}}">
+    </head>
+    <body>
+        <div class="full-width-div">
+            <div class="login-page-height">
+                <div class="d-none d-sm-block login-left-side">
+                    <div class="col-12 d-flex align-items-start">
+                        <div class="login-logo" >
+                            <img src="{{asset('assets/member/SocialPikeHTML/assets/images/SampleLogo.png')}}" class="img-fluid" alt="Logo image">
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center logo-text-1" >
+                        <div style="text-align:center;" >
+                            <label class="login-logo-text">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </label>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center logo-text-2" >
+                        <div style="text-align:center;" >
+                            <label class="login-logo-text">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 login-right-side pl-lg-5">
+                    <div class="row pt-3" >
+                        <div class="d-block d-xl-none d-md-none" style="margin:auto;">
+                            <div class="login-logo" >
+                                <img src="{{asset('assets/member/SocialPikeHTML/assets/images/SampleLogo.png')}}" class="img-fluid" alt="Logo image">
+                            </div>
+                        </div>
+                        <form class="form-inline" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+                            <div class="form-group col-12 col-lg-5 col-md-5 login-email-colform-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <input id='email' type="email" name='email' class="form-control login-input input-rounded login-email-resize" value="{{ old('email') }}" required placeholder="E-mail">
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
+                            <div class="col-12 col-lg-5 col-md-5 login-password-col form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <input type="password" class="form-control input-rounded login-email-resize" id="password" name='password' placeholder="Password" required>
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
+                                <a class="login-forgot-password" href="{{ route('password.request') }}"> Forgot Password? </a>
                             </div>
+                            <div class="col-12 col-lg-2 col-md-2 login-btn-col" >
+                                <button type="submit" class="btn login-page-btn login-button">LOGIN</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="d-block d-sm-none" >
+                        <hr>
+                    </div>
+                    <div class="row" >
+                        <div class="col-12 mt-lg-5 login-register-text">
+                            <h1>Register</h1>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+                        <div class="col-12" >
+                            <form class="register-input" method="POST" action="{{ route('register') }}">
+                                {{ csrf_field() }}
+                                <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
+                                    <input id="firstname" type="text" class="form-control input-rounded" name="firstname" value="{{ old('firstname') }}" placeholder="First Name">
+                                    @if ($errors->has('firstname'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('firstname') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
+                                    <input id="lastname" type="text" class="form-control input-rounded" name="lastname" value="{{ old('lastname') }}" placeholder="Last Name">
+                                    @if ($errors->has('lastname'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('lastname') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <input id="email" type="text" class="form-control input-rounded" name="email" value="{{ old('email') }}" placeholder="E-mail">
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                    <input id="password" type="text" class="form-control input-rounded" name="password" value="{{ old('password') }}" placeholder="Password">
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                    <input id="password-confirm" type="password" class="form-control input-rounded" name="password_confirmation" placeholder="Retype password">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label class="login-birthday-text">Birthday</label>
+                                    <div class="row login-birthday-row">
+                                        <select class="form-control register-date register-brithday-picker">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                        <select class="form-control register-month register-brithday-picker">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                        <select class="form-control register-year register-brithday-picker">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row login-gender-row">
+                                        <div class="form-check form-check-inline ml-3 mr-5">
+                                            <label class="form-check-label">
+                                            <input class="form-check-input" type="radio" name="" id="" value=""> Male
+                                            </label>
+                                        </div>
+                                        <div class="form-check form-check-inline ml-3 mr-5">
+                                            <label class="form-check-label">
+                                            <input class="form-check-input" type="radio" name="" id="" value=""> Female
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="create-account mt-5">
+                                    <button type="submit" class="btn login-page-btn createaccount-button">Create Account</button>
+                                </div>
+                            </form>
+                            <div class="login-or mt-5">
+                                <!-- OR -->
+                                <div class="row">
+                                    <div class="col-2 col-lg-2 col-xl-1 col-md-1">
+                                    </div>
+                                    <div class="col-3 col-lg-3 col-xl-3 col-md-4">
+                                        <hr>
+                                    </div>
+                                    <div class="col-2 col-lg-2 col-xl-2 col-md-2" style="text-align:center;">
+                                        <label>OR</label>
+                                    </div>
+                                    <div class="col-3 col-lg-3 col-xl-3 col-md-4">
+                                        <hr>
+                                    </div>
+                                    <div class="col-2 col-lg-2 col-xl-3 col-md-1">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                            <div class="facebook-login">
+                                <a href="#">
+                                    <div class="col-md-8 col-md-offset-4">
+                                        <a href="{{url('/redirect')}}">
+                                            <img src="{{asset('assets/member/SocialPikeHTML/assets/images/facebook-login.png')}}" class="img-fluid" alt="Logo imagesge">
+                                        </a>
+                                    </div>
                                 </a>
                             </div>
                         </div>
-                        <div class="col-md-8 col-md-offset-4">
-                            OR <br>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                              <a href="{{url('/redirect')}}" class="btn btn-primary">Login with Facebook</a>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+        <script type="text/javascript" src="{{asset('assets/member/SocialPikeHTML/node_modules/jquery/dist/jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/member/SocialPikeHTML/node_modules/popper.js/dist/umd/popper.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/member/SocialPikeHTML/node_modules/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+        <script type="text/javascript"></script>
+    </body>
+</html>
